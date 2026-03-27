e73a21e07504fdc9b380b116a2040a7ba21579af
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-27 10:43:17 -0700

    no longer needed to cache via this point

--- Sources/SPFKPlaylistData/Playlist+Elements.swift
+++ Sources/SPFKPlaylistData/Playlist+Elements.swift
@@ -2,24 +2,6 @@ import AppKit
 import SPFKBase
 
 extension Playlist {
-    public mutating func cache(cgImage: CGImage, for url: URL) throws {
-        guard let index = index(of: url) else {
-            throw NSError(file: #file, function: #function, description: "invalid url passed to function")
-        }
-
-        elements[index].mafDescription.imageDescription.cgImage = cgImage
-    }
-
-    public mutating func removeImage(at index: Int) throws {
-        guard elements.indices.contains(index) else {
-            throw NSError(description: "invalid index")
-        }
-
-        elements[index].mafDescription.imageDescription.cgImage = nil
-    }
-}
-
-extension Playlist {
     /// Refreshes security-scoped bookmark data for every element.
     public mutating func updateBookmarks() {
         for i in 0 ..< elements.count {
